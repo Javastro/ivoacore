ce1a9d0f94cf0fe05a0c4a92a7f4f00a6d3a790d
StephenL <stephen.lloyd@manchester.ac.uk>
2026-04-21 11:34:50 +0100

    Removed the experimental dependencies that seem to be included transitively

--- uws/build.gradle.kts
+++ uws/build.gradle.kts
@@ -10,11 +10,4 @@ dependencies {
     //Database persistence layer
     implementation("org.mapstruct:mapstruct:1.6.3")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
-
-    implementation("jakarta.inject:jakarta.inject-api:2.0.1")
-    implementation("org.hibernate.orm:hibernate-core:7.3.1.Final")
-    implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
-    implementation("org.postgresql:postgresql:42.7.10")
-
-    testRuntimeOnly("com.h2database:h2:2.3.232")
 }
