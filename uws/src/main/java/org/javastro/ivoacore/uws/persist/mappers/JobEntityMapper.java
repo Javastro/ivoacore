08ec9f407f872e0b63359bbccfe9559228d00443
StephenL <stephen.lloyd@manchester.ac.uk>
2026-04-22 14:21:09 +0100

    Tided up the conversion mapper class

--- uws/src/main/java/org/javastro/ivoacore/uws/persist/mappers/JobEntityMapper.java
+++ uws/src/main/java/org/javastro/ivoacore/uws/persist/mappers/JobEntityMapper.java
@@ -3,7 +3,6 @@ package org.javastro.ivoacore.uws.persist.mappers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.javastro.ivoacore.uws.BaseUWSJob;
 import org.javastro.ivoacore.uws.JobSpecification;
-import org.javastro.ivoacore.uws.SimpleLambdaJob;
 import org.javastro.ivoacore.uws.persist.UWSJobEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -23,10 +22,17 @@ public abstract class JobEntityMapper {
 
     @Mapping(target = "jobId", source = "ID")
     @Mapping(target = "jobSpecificationJson", expression = "java(serializeSpec(job.getJobSpecification()))")
-  //  @Mapping(target = "exceptionMessage", expression = "java(mapExceptionMessage(job))")
-  //  @Mapping(target = "exceptionType", expression = "java(mapExceptionType(job))")
+
     public abstract UWSJobEntity toEntity(BaseUWSJob job);
 
+    /**
+     * Serializes a given {@link JobSpecification} instance into its JSON string representation
+     * using the configured {@link ObjectMapper}.
+     *
+     * @param spec the {@link JobSpecification} instance to be serialized
+     * @return the JSON string representation of the provided {@link JobSpecification}
+     * @throws RuntimeException if serialization fails due to any underlying error
+     */
     protected String serializeSpec(JobSpecification spec) {
         try {
             return objectMapper.writeValueAsString(spec);
@@ -35,6 +41,14 @@ public abstract class JobEntityMapper {
         }
     }
 
+    /**
+     * Converts a {@link UWSJobEntity} instance into a {@link JobSpecification} by deserializing
+     * the JSON string stored in the {@code jobSpecificationJson} field of the provided entity.
+     *
+     * @param entity the {@link UWSJobEntity} containing the JSON representation of a {@link JobSpecification}
+     * @return the deserialized {@link JobSpecification} instance
+     * @throws RuntimeException if deserialization fails due to any underlying error
+     */
     public JobSpecification toSpecification(UWSJobEntity entity) {
         try {
             return objectMapper.readValue(entity.jobSpecificationJson, JobSpecification.class);
